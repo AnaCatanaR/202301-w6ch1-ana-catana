b8fcdcd63a66af36defa6d61a785a2d326b7d1a7
AnaCatanaR <anacatana.ac@gmail.com>
2023-02-14 23:21:22 +0100

    Fix styles of TaskCard component

---
+++ src/data/toDos.tsx
@@ -0,0 +1,56 @@
+import { TodosListStructure } from "../types";
+
+const toDos: TodosListStructure = [
+  {
+    id: 1,
+    name: "RTFM",
+    isDone: false,
+  },
+  {
+    id: 2,
+    name: "Render Api",
+    isDone: false,
+  },
+  {
+    id: 3,
+    name: "Buy TIME",
+    isDone: false,
+  },
+  {
+    id: 4,
+    name: "Sleep",
+    isDone: false,
+  },
+  {
+    id: 5,
+    name: "Change Favicon and Title",
+    isDone: false,
+  },
+  {
+    id: 6,
+    name: "Read WAI-AREA Roles",
+    isDone: true,
+  },
+  {
+    id: 7,
+    name: "Change the local branch name",
+    isDone: true,
+  },
+  {
+    id: 8,
+    name: "Understand createSlice",
+    isDone: false,
+  },
+  {
+    id: 9,
+    name: "Do a custom hook",
+    isDone: false,
+  },
+  {
+    id: 10,
+    name: "Pass validators",
+    isDone: true,
+  },
+];
+
+export default toDos;
