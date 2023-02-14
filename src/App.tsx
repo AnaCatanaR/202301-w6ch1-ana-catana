070cc29b55534059a0fb9124fd26ecf6d00eabb0
AnaCatanaR <anacatana.ac@gmail.com>
2023-02-15 00:15:43 +0100

    Add App component to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-function App() {
-  return <div></div>;
+import "./App.css";
+import Header from "./components/Header/Header";
+import TaskList from "./components/TaskList/TaskList";
+
+function App(): JSX.Element {
+  return (
+    <>
+      <Header />
+      <TaskList />
+    </>
+  );
 }
 
 export default App;
