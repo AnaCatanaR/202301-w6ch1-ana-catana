03423aeb5b47e97ff1d5fe6def00bcaa13e7b8e1
AnaCatanaR <anacatana.ac@gmail.com>
2023-02-14 21:38:48 +0100

    Create Task Card component

---
+++ src/components/TaskCard/TaskCard.tsx
@@ -0,0 +1,29 @@
+import { TodoStructure } from "../../types";
+
+interface CardTaskProps {
+  toDo: TodoStructure;
+}
+
+const TaskCard = ({
+  toDo: { id, isDone, name },
+}: CardTaskProps): JSX.Element => {
+  return (
+    <>
+      <article className="card-task">
+        <div className="card-task__task">
+          <span className="">
+            {id} {name}
+          </span>
+          <span>{isDone}✓</span>
+        </div>
+
+        <div className="card-task-buttons">
+          <button>Delete</button>
+          <button>Edit</button>
+        </div>
+      </article>
+    </>
+  );
+};
+
+export default TaskCard;
